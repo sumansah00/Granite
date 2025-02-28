59a41d7da36b47f3ae6999c65040f40b65107794
Suman Sahoo <suman.sahoo@bigbinary.com>
2025-02-28 11:14:01 +0530

    Added show page for a task

--- app/javascript/src/components/Tasks/Table/index.jsx
+++ app/javascript/src/components/Tasks/Table/index.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 
-import Header from "./Header";
-import Row from "./Row";
+import PropTypes from "prop-types";
 
-const Table = ({ data }) => (
-  <div className="inline-block min-w-full">
-    <table className="min-w-full border-collapse border border-gray-300">
-      <Header />
-      <Row data={data} />
-    </table>
-  </div>
+import { Tooltip } from "components/commons";
+
+const Row = ({ data, showTask }) => (
+  <tbody className="divide-y divide-gray-200 bg-white">
+    {data.map(rowData => (
+      <tr key={rowData.id}>
+        <td className="space-x-5 border-r border-gray-300 px-4 py-2.5 text-sm font-medium capitalize">
+          <Tooltip tooltipContent={rowData.title}>
+            <span>{rowData.title}</span>
+          </Tooltip>
+        </td>
+        <td className="cursor-pointer px-6 py-4 text-right text-sm font-medium leading-5">
+          <a className="text-indigo-600" onClick={() => showTask(rowData.slug)}>
+            Show
+          </a>
+        </td>
+      </tr>
+    ))}
+  </tbody>
 );
 
-export default Table;
+Row.propTypes = {
+  data: PropTypes.array.isRequired,
+  showTask: PropTypes.func,
+};
+
+export default Row;
