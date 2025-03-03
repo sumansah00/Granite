17ace397b8b03bdd08eed177646acb7cfbecbc74
Suman Sahoo <suman.sahoo@bigbinary.com>
2025-03-03 11:31:34 +0530

    Added a user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_27_065315) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_03_060035) do
   create_table "tasks", force: :cascade do |t|
     t.text "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug", null: false
+    t.integer "assigned_user_id"
     t.index ["slug"], name: "index_tasks_on_slug", unique: true
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "tasks", "users", column: "assigned_user_id"
 end
