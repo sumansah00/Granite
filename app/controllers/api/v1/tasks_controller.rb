15cd56f6c8995b111806cca6c21015db74673190
Suman Sahoo <suman.sahoo@bigbinary.com>
2025-03-04 11:32:33 +0530

    Refactor task show action to include only specific attributes of the assigned user

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -17,7 +17,7 @@ module Api
       end
 
       def show
-        render_json({ task: @task, assigned_user: @task.assigned_user })
+        render_json({ task: @task.as_json(include: { assigned_user: { only: %i[name id] } }) })
       end
 
       def update
